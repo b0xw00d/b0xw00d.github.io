b0922f6960f5bc2dc4c1cd5556a75c5867c4ae7e
b0xw00d <chasenutile@gmail.com>
2015-09-05 20:41:55 -0500

    new chase_rogers blog. bwahahahaha

--- README.md
+++
@@ -1,4 +0,0 @@
-b0xw00d.github.io
-=================
-
-My phase 0 github pages site!
---
+++ js/experience.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+  $("#experience-button").click(function(){
+    $("#work-experience").toggle(500);
+    // $("#work").toggle(500);
+  });
+
+});
+
