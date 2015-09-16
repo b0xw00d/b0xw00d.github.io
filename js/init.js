594c25df1baf0c36d74fd930c74e646cdfaf278b
b0xw00d <chasenutile@gmail.com>
2015-09-15 22:20:24 -0500

    styling on mobile slider

--- js/init.js
+++ js/init.js
@@ -2,6 +2,7 @@
   $(function(){
 
     $('.button-collapse').sideNav();
+    $(".dropdown-button").dropdown({ hover: false });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
